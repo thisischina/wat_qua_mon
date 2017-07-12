2c25924c9d333dcb7e87a84d57b43f739fc10150
CarlosJones <18912144596@163.com>
2017-07-12 10:56:40 +0800

    首页地图的模拟

--- src/main/webapp/assets/js/map.js
+++ src/main/webapp/assets/js/map.js
@@ -1,6 +1,6 @@
 $(function(){
     var map = new MachMap();
-})   
+});
 var points;
 function MachMap() {
     this.map = this.initMap(this.containerId);
@@ -14,19 +14,31 @@ var heatmapOverlay = new BMapLib.HeatmapOverlay({"radius":20});
 // 根据地图容器的ID,初始化地图
 MachMap.prototype.initMap = function (containerID) {
     var map = new BMap.Map("map_container");            // 创建地图实例
-    var point = new BMap.Point(116.418261, 39.921984);  // 创建点坐标(目前为双沟镇政府)
+    var point = new BMap.Point(116.418261, 39.921984);  // 创建中心点坐标
+    //创建覆盖物
+    var pt = new BMap.Point(116.417, 39.909);
+    var myIcon = new BMap.Icon("../assets/images/sewage.png", new BMap.Size(50,50));
+    var marker = new BMap.Marker(pt,{icon:myIcon});  // 创建覆盖物
+    var infoWindow = new BMap.InfoWindow(sContent);  // 创建信息窗口对象
+    marker.addEventListener("click", function(){     //覆盖物的信息窗口
+        this.openInfoWindow(infoWindow);
+        //图片加载完毕重绘infowindow
+        document.getElementById('imgDemo').onload = function (){
+            infoWindow.redraw();   //防止在网速较慢，图片未加载时，生成的信息框高度比图片的总高度小，导致图片部分被隐藏
+        };
+    });
     map.centerAndZoom(point, 15);                       // 初始化地图，设置中心点坐标和地图级别
 
     map.enableScrollWheelZoom(true);                //鼠标滑动轮子可以滚动
     map.enableKeyboard(true);                       //启用键盘操作
-    map.setMapType(BMAP_HYBRID_MAP);                //设置为卫星图
 
     map.addControl(new BMap.MapTypeControl());      //添加地图类型控件
     map.addControl(new BMap.NavigationControl());   // 添加平移缩放控件
     map.addControl(new BMap.ScaleControl());        // 添加比例尺控件
     map.addControl(new BMap.OverviewMapControl());  //添加缩略地图控件
+    map.addOverlay(marker);              // 将覆盖物添加到地图中
     if(!isSupportCanvas()){                         //添加热力图图层
-    	alert('热力图目前只支持有canvas支持的浏览器,您所使用的浏览器不能使用热力图功能~')
+    	alert('热力图目前只支持有canvas支持的浏览器,您所使用的浏览器不能使用热力图功能~');
     }else{       
         map.addOverlay(heatmapOverlay);
         heatmapOverlay.setDataSet({data:points,max:100});
@@ -154,6 +166,76 @@ MachMap.prototype.initMap = function (containerID) {
     {"lng":116.424579,"lat":39.914987,"count":57},
     {"lng":116.42076,"lat":39.915251,"count":70},
     {"lng":116.425867,"lat":39.918989,"count":8}];
+var sContent =
+    "<table width=\"100%\"  border=\"1\" cellspacing=\"0\" cellpadding=\"0\">"+
+    "<tr>"+
+    "<td><table background=\"images/index/b06_w.gif\" style=\"margin-top:5px; margin-left:18px;\" width=\"317\" height=\"270\" border=\"0\" cellpadding=\"0\" cellspacing=\"6\">"+
+    "<tr>"+
+    "<td style=\"height: 209px\" valign=\"top\">"+
+    "<table width=\"317\" border=\"1\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\">"+
+    "<tr>"+
+    "<td valign=\"top\"><div align=\"center\" class=\"style_2\"><strong><span id=\"title\"></span>水质自动监测站</strong></div></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td valign=\"top\">"+
+    "<table width=\"315\" border=\"1\" align=\"center\" cellpadding=0 cellspacing=0 class=\"table1 style_2\">"+
+    "<tr>"+
+    "<td width=\"81\" height=\"30\" bgcolor=\"#f0f0f0\" align=\"center\"><strong>测量时间</strong></td>"+
+    "<td width=\"82\" height=\"20\" bgcolor=\"#FFFFFF\" align=\"center\"><strong>项目</strong></td>"+
+    "<td width=\"44\" bgcolor=\"#f0f0f0\" align=\"center\"><strong>测量值</strong></td>"+
+    "<td width=\"53\" bgcolor=\"#FFFFFF\" align=\"center\"><strong>水质类别</strong></td>"+
+    "<td width=\"53\" bgcolor=\"#FFFFFF\" align=\"center\"><strong>Ⅲ类标准</strong></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td width=\"81\" height=\"30\" rowspan=\"2\" bgcolor=\"#f0f0f0\" class=\"style_2\" align=\"center\"><strong><span id=\"date\"></span></strong></td>"+
+    "<td width=\"82\" height=\"20\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>pH</strong></div></td>"+
+    "<td width=\"44\" bgcolor=\"#f0f0f0\" align=\"center\"><strong><span id=\"pH\"></span></strong></td>"+
+    "<td width=\"53\" bgcolor=\"#FFFFFF\" align=\"center\"><strong><span id=\"levelpH\"></span></strong></td>"+
+    "<td width=\"53\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>6-9</strong></div></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td height=\"20\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>溶解氧</strong></div></td>"+
+    "<td bgcolor=\"#f0f0f0\" align=\"center\"><strong><span id=\"DO\"></span></strong></td>"+
+    "<td bgcolor=\"#FFFFFF\" align=\"center\"><strong><span id=\"levelDO\"></span></strong></td>"+
+    "<td bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>≥5</strong></div></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td id=\"td_time\" rowspan=\"3\" bgcolor=\"#f0f0f0\" align=\"center\"><strong><span id=\"time\"></span></strong></td>"+
+    "<td height=\"20\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>氨氮</strong></div></td>"+
+    "<td bgcolor=\"#f0f0f0\" align=\"center\"><strong><span id=\"NH3N\"></span></strong></td>"+
+    "<td bgcolor=\"#FFFFFF\" align=\"center\"><strong><span id=\"levelNH3N\"></span></strong></td>"+
+    "<td bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>≤1.0</strong></div></td>"+
+    "</tr>"+
+    "<tr id=\"tr_cod\">"+
+    "<td id=\"td_cod\" height=\"20\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>高锰酸盐指数</strong></div></td>"+
+    "<td id=\"td_codvalue\" bgcolor=\"#f0f0f0\" align=\"center\"><strong><span id=\"COD\"></span></strong></td>"+
+    "<td id=\"td_codg\" bgcolor=\"#FFFFFF\" align=\"center\"><strong><span id=\"levelCOD\"></span></strong></td>"+
+    "<td id=\"td_codm\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>≤6</strong></div></td>"+
+    "</tr>"+
+    "<tr id=\"tr_toc\">"+
+    "<td height=\"20\" bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>总有机碳</strong></div></td>"+
+    "<td bgcolor=\"#f0f0f0\" align=\"center\"><strong><span id=\"TOC\"></span></strong></td>"+
+    "<td bgcolor=\"#FFFFFF\" align=\"center\"><strong><span id=\"levelTOC\"></span></strong></td>"+
+    "<td bgcolor=\"#FFFFFF\" align=\"center\"><div align=\"center\"><strong>-</strong></div></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td bgcolor=\"#f0f0f0\" style=\"height: 20px\" align=\"center\"><div align=\"center\"><strong>断面属性</strong></div></td>"+
+    "<td colspan=\"4\" bgcolor=\"#FFFFFF\" style=\"height: 20px\" align=\"center\"><strong><span id=\"propertysta\"></span></strong></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td height=\"20\" bgcolor=\"#f0f0f0\" align=\"center\"><div align=\"center\"><strong>站点情况</strong></div></td>"+
+    "<td colspan=\"4\" bgcolor=\"#FFFFFF\" align=\"center\"><strong><a href=\"#\" onfocus=\"this.blur();\"><font style=\"color:Black\"><span id=\"statussta\" onmousedown=\"showstationinfo()\"></span></font></a></strong></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td height=\"20\" bgcolor=\"#f0f0f0\" align=\"center\"><div align=\"center\"><strong>站点简介</strong></div></td>"+
+    "<td colspan=\"4\" bgcolor=\"#FFFFFF\" align=\"center\"><strong><a href=\"#\" onfocus=\"this.blur();\"><font style=\"color:Black\"><span id=\"stationinfo\" onmousedown=\"showstationinfo_1()\"></span></font></a></strong></td>"+
+    "</tr>"+
+    "<tr>"+
+    "<td height=\"20\" bgcolor=\"#f0f0f0\" align=\"center\"><div align=\"center\"><strong>备注</strong></div></td>"+
+    "<td colspan=\"4\" bgcolor=\"#FFFFFF\" align=\"center\"><strong> * 为待更新仪器    - 为待检修仪器<br />pH无量纲，其他参数单位mg/L</strong></td>"+
+    "</tr>"+
+    "</table>"
+;
 
 
 
