13abba5a0927738558f573e6e43652eddbcf5ac9
lekoxnfx <lekoxnfx@qq.com>
2017-07-25 15:59:25 +0800

    修改spring-mina.xml,目前已经能接收到模拟终端上传的数据。

--- src/main/java/com/hd/ibus/socketserver/MinaCodeFactory.java
+++ src/main/java/com/hd/ibus/socketserver/MinaCodeFactory.java
@@ -4,16 +4,30 @@ import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFactory;
 import org.apache.mina.filter.codec.ProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolEncoder;
+import org.apache.mina.filter.codec.textline.LineDelimiter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.codec.textline.TextLineDecoder;
+import org.apache.mina.filter.codec.textline.TextLineEncoder;
+
+import java.nio.charset.Charset;
 
 /**
  * Created by lekoxnfx on 2017/7/13.
  */
 public class MinaCodeFactory implements ProtocolCodecFactory {
+    private final MinaEncoder encoder;
+    private final MinaDecoder decoder;
+
+    MinaCodeFactory(){
+        this.encoder = new MinaEncoder();
+        this.decoder = new MinaDecoder();
+    }
+
     public ProtocolEncoder getEncoder(IoSession ioSession) throws Exception {
-        return new MinaEncoder();
+        return encoder;
     }
 
     public ProtocolDecoder getDecoder(IoSession ioSession) throws Exception {
-        return new MinaDecoder();
+        return decoder;
     }
 }
